ae95ac6ff28ae1a34de2cc8875056c8883b98481
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2005-09-13 17:50:48 +0000

    support single zip file

--- trunk/web/htdocs/mkfirm.c
+++ trunk/web/htdocs/mkfirm.c
@@ -2,7 +2,9 @@
  *
  * Original version by Petr Novak for SMC7004ABR rev.2
  * Modified to support SMC7004VWBR PN720.x version by BLFC from Openline ISP.
- * Heavily modified to support many different devices by Stefan Weil.
+ *
+ * Heavily modified to support many different devices and enhanced firmware
+ * formats by Stefan Weil.
  *
  * This software is distributed under the GNU public license (GPL).
  */
@@ -10,12 +12,15 @@
 #include <stdio.h>
 #include <stdlib.h>	// exit
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h>	// getopt
 #include <sys/types.h>
 #include <fcntl.h>
 
 typedef unsigned long uint32_t;
 
+#if !defined(O_BINARY)
+# define O_BINARY 0
+#endif
 
 /**********************************************************************
  *
@@ -80,10 +85,13 @@ typedef struct {
 	size_t soho_size;
 } device_t;
 
+#define KB 1024
+
 static const device_t device[] = {
 	{"BRNABR", "SMC7004ABR", 0x30000, 0xbb800},
 	{"BRNAW", "SMC7004VWBR", 0x30000, 0xbb800},
-	{"BRN154BAS", "Sinus 154 DSL Basic SE", 0x30000, 0xbb800},
+	{"BRN154BAS", "Sinus 154 DSL Basic SE", 832 * KB, 896 * KB},
+	//{"BRN154BAS", "Sinus 154 DSL Basic SE", 0x30000, 0xbb800},
 	{"BRNDTBAS3", "Sinus 154 DSL Basic 3", 0x30000, 0xbb800},
 	{"BRN154DSL", "Sinus 154 DSL", 0x30000, 0xbb800},
 	{"BRN154KOM", "Sinus 154 DSL Komfort", 0x30000, 0xbb800},
@@ -91,7 +99,7 @@ static const device_t device[] = {
 };
 
 /* buffer must be large enough to contain pfs + soho + signature */
-static unsigned char buffer[0x100000];
+static unsigned char buffer[0x200000];
 
 static unsigned char signature[10];
 
@@ -100,42 +108,93 @@ static const char *program_name;
 static void usage(void)
 {
 	const device_t *dev;
-	fprintf(stderr, "Usage: %s <magic> <pfs> <soho>\n", program_name);
-	fprintf(stderr, "<magic>: any of the following\n");
+	fprintf(stderr, "Usage: %s [-h|-?]\n", program_name);
+	fprintf(stderr, "  or:  %s [-o <outfile>] [-l <blocksize>] -m <magic> <zipfile>\n", program_name);
+	fprintf(stderr, "  or:  %s [-o <outfile>] [-l <blocksize>] -m <magic> <pfsfile> <sohofile>\n", program_name);
+	fprintf(stderr, "<magic>:     any of the following\n");
 	for (dev = device; ; dev++) {
 		const char *magic = dev->magic;
 		const char *description = dev->description;
 		if (magic == 0) break;
 		fprintf(stderr, "\t%-10s%s\n", magic, description);
 	}
+	fprintf(stderr, "<zipfile>:   zipped file with code or user interface\n");
+	fprintf(stderr, "<pfsfile>:   zipped file with user interface (pfs.img)\n");
+	fprintf(stderr, "<sohofile>:  zipped file with code (soho.bin)\n");
+	fprintf(stderr, "<outfile>:   write result into this file\n");
+	fprintf(stderr, "<blocksize>: size of flash blocks (default: 65536)\n");
+}
+
+static unsigned char *write_data(const char *filename, char *buffer, size_t max_size)
+{
+	unsigned long *p;
+	unsigned long crc;
+	unsigned long len;
+	int fd = open(filename, O_RDONLY|O_BINARY);
+	if (fd < 0) {
+		perror(filename);
+		exit(1);
+	}
+	len = read(fd, buffer, max_size);
+	close(fd);
+	fprintf(stderr, "%s has %lu (0x%lx) bytes, %lu bytes left\n", filename, len, len, max_size - len);
+	crc = comp_crc(buffer, len);
+	max_size = ((len + 0x7fff) & 0xffff8000);
+	fprintf(stderr, "%s uses %lu (0x%lx) bytes = %u KiB, %lu bytes left\n", filename, max_size, max_size, max_size / 1024, max_size - len);
+	if (strncmp(buffer, "PK", 2) != 0) {
+		fprintf(stderr, "%s is no zip file\n", filename);
+	}
+	p = (unsigned long *)(buffer + max_size - 3 * 4);
+	*p++ = len;
+	*p++ = 0x12345678;
+	*p++ = crc;
+	return (unsigned char *)p;
 }
 
 int main(int argc, char *argv[])
 {
-	unsigned long len;
-	unsigned long crc;
-	unsigned long *p;
+	unsigned char *p;
 	int fd;
-
-	const char *magic;
+	
+	const char *magic = 0;
 	const char *pfs_file;
 	const char *soho_file;
+	const char *output_file = 0;
 
 	const device_t *dev;
 	size_t pfs_size;
 	size_t soho_size;
 	size_t total_size;
+	size_t block_size = 65536;
 
-	program_name = *argv++;
+	program_name = argv[0];
 
-	if (argc != 4) {
+	for (;;) {
+		int option = getopt(argc, argv, "?hmo");
+		if (option == -1) {
+			break;
+		} else if (option == '?' || option == 'h') {
+			usage();
+			exit(0);
+		} else if (option == 'l') {
+			block_size = strtoul(argv[optind++], 0, 0);
+		} else if (option == 'm') {
+			magic = argv[optind++];
+		} else if (option == 'o') {
+			output_file = argv[optind++];
+		} else {
+			usage();
+			exit(1);
+		}
+	}
+
+	if (((optind + 1 != argc) && (optind + 2 != argc)) || magic == 0) {
 		usage();
 		exit(1);
 	}
 
-	magic = *argv++;
-	pfs_file = *argv++;
-	soho_file = *argv++;
+	pfs_file = argv[optind++];
+	soho_file = argv[optind++];
 
 	for (dev = device; ; dev++) {
 		if (dev->magic == 0) {
@@ -159,37 +218,26 @@ int main(int argc, char *argv[])
 	makecrc();
 	memset(buffer, 0xff, total_size);
 
-	fd = open(pfs_file, O_RDONLY);
-	if (fd < 0) {
-		perror("open PFS (192K) SOHO (576K)");
-		exit(1);
+	p = buffer;
+	p = write_data(pfs_file, p, pfs_size);
+
+	if (soho_file != 0) {
+		p = write_data(soho_file, p, soho_size);
 	}
-	len = read(fd, buffer, pfs_size);
-	close(fd);
-	fprintf(stderr, "Web (PFS) has %lu (0x%lx) bytes, %lu bytes left\n", len, len, pfs_size - len);
-	crc = comp_crc(buffer, len);
-	p = (unsigned long *)(buffer + pfs_size - 3 * 4);
-	p[0] = len;
-	p[1] = 0x12345678;
-	p[2] = crc;
 
-	fd = open(soho_file, O_RDONLY);
-	if (fd < 0) {
-		perror("open SOHO (576K)");
-		exit(1);
-	}
-	len = read(fd, buffer + pfs_size, soho_size);
-	close(fd);
-	fprintf(stderr, "Firmware (SOHO) has %lu (0x%lx) bytes, %lu bytes left\n", len, len, soho_size - len);
-	crc = comp_crc(buffer + pfs_size, len);
-	p = (unsigned long *)(buffer + pfs_size + soho_size - 3 * 4);
-	p[0] = len;
-	p[1] = 0x12345678;
-	p[2] = crc;
+	memcpy(p, signature, 10);
 
-	memcpy(buffer + pfs_size + soho_size, signature, 10);
-
-	if (!isatty(1)) {
+	total_size = (p - buffer) + sizeof(signature);
+	
+	if (output_file != 0) {
+		fd = open(output_file, O_CREAT|O_WRONLY|O_TRUNC|O_BINARY, 0666);
+		if (fd < 0) {
+			perror(pfs_file);
+			exit(1);
+		}
+		write(fd, buffer, total_size);
+		close(fd);
+	} else if (!isatty(1)) {
 		write(1, buffer, total_size);
 	}
 
