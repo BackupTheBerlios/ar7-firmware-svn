874e274fccdccfe7ed1b975ca4af562c866129b9
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2007-01-16 17:59:41 +0000

    cvs update

--- qemu/branches/head/vl.h
+++ qemu/branches/head/vl.h
@@ -302,6 +302,7 @@ typedef struct CharDriverState {
     QEMUBH *bh;
 } CharDriverState;
 
+CharDriverState *qemu_chr_open(const char *filename);
 void qemu_chr_printf(CharDriverState *s, const char *fmt, ...);
 int qemu_chr_write(CharDriverState *s, const uint8_t *buf, int len);
 void qemu_chr_send_event(CharDriverState *s, int event);
@@ -824,6 +825,8 @@ void i440fx_set_smm(PCIDevice *d, int val);
 int piix3_init(PCIBus *bus, int devfn);
 void i440fx_init_memory_mappings(PCIDevice *d);
 
+int piix4_init(PCIBus *bus, int devfn);
+
 /* openpic.c */
 typedef struct openpic_t openpic_t;
 void openpic_set_irq(void *opaque, int n_IRQ, int level);
@@ -834,6 +837,9 @@ openpic_t *openpic_init (PCIBus *bus, int *pmem_index, int nb_cpus,
 typedef struct HeathrowPICS HeathrowPICS;
 void heathrow_pic_set_irq(void *opaque, int num, int level);
 HeathrowPICS *heathrow_pic_init(int *pmem_index);
+
+/* gt64xxx.c */
+PCIBus *pci_gt64120_init(void *pic);
 
 #ifdef HAS_AUDIO
 struct soundhw {
@@ -1055,6 +1061,9 @@ extern QEMUMachine heathrow_machine;
 
 /* mips_r4k.c */
 extern QEMUMachine mips_machine;
+
+/* mips_malta.c */
+extern QEMUMachine mips_malta_machine;
 
 /* mips_timer.c */
 extern void cpu_mips_clock_init(CPUState *);
