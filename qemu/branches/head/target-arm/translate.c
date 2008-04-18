73d9a8d6c8dd4761656c034cbba006cd7d4460cb
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2008-04-18 21:12:41 +0000

    ------------------------------------------------------------------------ r4209 | aurel32 | 2008-04-13 18:08:44 +0200 (So, 13 Apr 2008) | 6 lines
    Fix keyboard emulation for ARM versatile board:
    - 0xab is actually a keyboard reply. It should not be escaped.
    - Because of translated value 0x41, translated to raw conversion is not
      a bijection. Instead of creating two translation tables, test for
      s->translate before writing this value.
    
    ------------------------------------------------------------------------
    r4210 | balrog | 2008-04-14 04:19:57 +0200 (Mo, 14 Apr 2008) | 6 lines
    
    Fix ARM VFP related segfaults.
    
    The fconst change is partly a guess.
    VFP_OP1 is still unused.
    An ARMv6 target still won't boot.
    
    ------------------------------------------------------------------------
    r4211 | balrog | 2008-04-14 21:14:06 +0200 (Mo, 14 Apr 2008) | 2 lines
    
    Correctly restore pc after an exception occured in Thumb mode.
    
    ------------------------------------------------------------------------
    r4212 | balrog | 2008-04-14 22:27:51 +0200 (Mo, 14 Apr 2008) | 2 lines
    
    Move the excess of arm_load_kernel() parameters into a struct.
    
    ------------------------------------------------------------------------
    r4213 | balrog | 2008-04-14 23:05:22 +0200 (Mo, 14 Apr 2008) | 7 lines
    
    Add basic OMAP2 chip support.
    
    Add the OMAP242x (arm1136 core) initialisation with basic on-chip
    peripherals and update OMAP1 peripherals which are re-used in OMAP2.
    Make palmte.c and sd.c errors go to stderr.
    Allow disabling SD chipselect.
    
    ------------------------------------------------------------------------
    r4214 | balrog | 2008-04-14 23:28:11 +0200 (Mo, 14 Apr 2008) | 5 lines
    
    Add TSC2301 touchscreen & keypad controller.
    
    Add also a facility for setting up touchscreen calibration data per-board
    based on calibration data generated by tslib.
    
    ------------------------------------------------------------------------
    r4215 | balrog | 2008-04-14 23:57:44 +0200 (Mo, 14 Apr 2008) | 8 lines
    
    Nokia N800 machine support (ARM).
    
    Also add various peripherals: two miscellaneous Nokia CBUS chips,
    EPSON S1D13745 LCD/TV remote-framebuffer controller,
    TWL92230 - standard OMAP2 power management companion chip on i2c.
    Generic OneNAND flash memory,
    TMP105 temperature sensor on i2c.
    
    ------------------------------------------------------------------------
    r4216 | balrog | 2008-04-17 01:07:32 +0200 (Do, 17 Apr 2008) | 2 lines
    
    Use qemu time/date functions for the Menelaus RTC.
    
    ------------------------------------------------------------------------
    r4217 | balrog | 2008-04-17 01:17:02 +0200 (Do, 17 Apr 2008) | 2 lines
    
    Save the exact SWI return address in Thumb mode.
    
    ------------------------------------------------------------------------
    r4218 | balrog | 2008-04-17 01:37:15 +0200 (Do, 17 Apr 2008) | 2 lines
    
    Allow a custom unlock address in CFI02 flash (Jan Kiszka).
    
    ------------------------------------------------------------------------
    r4219 | balrog | 2008-04-17 01:45:36 +0200 (Do, 17 Apr 2008) | 2 lines
    
    Emulate address wrap in CFI02 chips mapping (Jan Kiszka).
    
    ------------------------------------------------------------------------
    r4220 | balrog | 2008-04-17 01:58:02 +0200 (Do, 17 Apr 2008) | 2 lines
    
    Optimize consecutive CFI02 writes by remapping memory lazily (Jan Kiszka).
    
    ------------------------------------------------------------------------

--- qemu/branches/head/target-arm/translate.c
+++ qemu/branches/head/target-arm/translate.c
@@ -905,11 +905,6 @@ static inline void gen_set_pc_im(uint32_t val)
     store_cpu_field(tmp, regs[15]);
 }
 
-static inline void gen_set_pc_T0(void)
-{
-    tcg_gen_st_i32(cpu_T[0], cpu_env, offsetof(CPUState, regs[15]));
-}
-
 static inline void gen_movl_reg_TN(DisasContext *s, int reg, int t)
 {
     TCGv tmp;
@@ -1009,7 +1004,7 @@ static inline void gen_vfp_##name(int dp)                             \
         gen_helper_vfp_##name##s(cpu_F0s, cpu_F0s, cpu_F1s, cpu_env); \
 }
 
-#define VFP_OP1i(name)                               \
+#define VFP_OP1(name)                               \
 static inline void gen_vfp_##name(int dp, int arg)  \
 {                                                   \
     if (dp)                                         \
@@ -1068,9 +1063,9 @@ static inline void gen_vfp_cmpe(int dp)
 static inline void gen_vfp_F1_ld0(int dp)
 {
     if (dp)
-        tcg_gen_movi_i64(cpu_F0d, 0);
+        tcg_gen_movi_i64(cpu_F1d, 0);
     else
-        tcg_gen_movi_i32(cpu_F0s, 0);
+        tcg_gen_movi_i32(cpu_F1s, 0);
 }
 
 static inline void gen_vfp_uito(int dp)
@@ -3117,7 +3112,7 @@ static int disas_vfp_insn(CPUState * env, DisasContext *s, uint32_t insn)
                         else
                             i |= 0x800;
                         n |= i << 19;
-                        tcg_gen_movi_i32(cpu_F0d, ((uint64_t)n) << 32);
+                        tcg_gen_movi_i32(cpu_F0s, n);
                     }
                     break;
                 case 15: /* extension space */
@@ -3486,7 +3481,7 @@ static int gen_set_psr_T0(DisasContext *s, uint32_t mask, int spsr)
 static void gen_exception_return(DisasContext *s)
 {
     TCGv tmp;
-    gen_set_pc_T0();
+    gen_movl_reg_T0(s, 15);
     tmp = load_cpu_field(spsr);
     gen_set_cpsr(tmp, 0xffffffff);
     dead_tmp(tmp);
@@ -8497,7 +8492,7 @@ static void disas_thumb_insn(CPUState *env, DisasContext *s)
         if (cond == 0xf) {
             /* swi */
             gen_set_condexec(s);
-            gen_set_pc_im(s->pc | 1);
+            gen_set_pc_im(s->pc);
             s->is_jmp = DISAS_SWI;
             break;
         }
